83c9e4fcb23a835bbd6887c5ea4b9beb50c52268
Emmanuel Hansen <emmausssss@gmail.com>
2023-08-06 12:53:01 +0000

    move game view to new activity

--- src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/Icons.kt
+++ src/RyujinxAndroid/app/src/main/java/org/ryujinx/android/Icons.kt
@@ -1,8 +1,6 @@
 package org.ryujinx.android
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
