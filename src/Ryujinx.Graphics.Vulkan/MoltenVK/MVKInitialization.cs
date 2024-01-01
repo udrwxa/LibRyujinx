c1d19a1ada3fccb2cb0ce94024064f8edb921dde
Isaac Marovitz <isaacryu@icloud.com>
2024-01-01 15:35:30 -0800

    iOS - Platform Checks
    Co-authored-by: riperiperi <rhy3756547@hotmail.com>

--- src/Ryujinx.Graphics.Vulkan/MoltenVK/MVKInitialization.cs
+++ src/Ryujinx.Graphics.Vulkan/MoltenVK/MVKInitialization.cs
@@ -6,6 +6,7 @@ using System.Runtime.Versioning;
 namespace Ryujinx.Graphics.Vulkan.MoltenVK
 {
     [SupportedOSPlatform("macos")]
+    [SupportedOSPlatform("ios")]
     public static partial class MVKInitialization
     {
         [LibraryImport("libMoltenVK.dylib")]
