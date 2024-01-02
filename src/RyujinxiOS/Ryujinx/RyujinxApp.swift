dc9437aa6c92785e4d3036a2a93b7517fefa74d1
Isaac Marovitz <isaacryu@icloud.com>
2024-01-01 16:35:10 -0800

    Initialize LibRyujinx on App Start

--- src/RyujinxiOS/Ryujinx/RyujinxApp.swift
+++ src/RyujinxiOS/Ryujinx/RyujinxApp.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import LibRyujinx
 
 @main
 struct RyujinxApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    let documentsDirectory = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                    initialize(strdup(documentsDirectory.path(percentEncoded: false)))
+                }
         }
     }
 }
